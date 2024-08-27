8a270f3253d21d2f7baeca40f1276b084b65bcda
Anthony Rubio <rubionn27@example.com>
2024-08-27 17:04:22 -0500

    Add: Enhace code quality and remove unused code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,69 +17,70 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const PrinterTemplate(),
+      home: FutureBuilder(
+        future: ZebraUtil.getPrinterInstance(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final printer = snapshot.data as ZebraPrinter;
+          return PrinterTemplate(printer);
+        },
+      ),
     );
   }
 }
 
 class PrinterTemplate extends StatefulWidget {
-  const PrinterTemplate({super.key});
-
+  const PrinterTemplate(this.printer, {super.key});
+  final ZebraPrinter printer;
   @override
   State<PrinterTemplate> createState() => _PrinterTemplateState();
 }
 
 class _PrinterTemplateState extends State<PrinterTemplate> {
   late ZebraPrinter zebraPrinter;
-  List<Map<String,String>> printers = [];
+  late ZebraPrinterNotifier notifier;
 
   @override
   void initState() {
-    createInstance();
-    super.initState();
-  }
-
-  void createInstance() async {
-    zebraPrinter = await ZebraUtil.getPrinterInstance(
-        onPrinterFound: (name, ipAddress, isWifi) {
-          setState(() {
-            printers.add({"ipAddress":ipAddress, "name": name});
-          });
-          print("onPrinterFound: $name, $ipAddress, $isWifi");
-        },
-        onPrinterDiscoveryDone: () => print("onPrinterDiscoveryDone"),
-        onDiscoveryError: (onErrorCode, onErrorText) =>
-            print("onDiscoveryError: $onErrorCode, $onErrorText"),
-        onChangePrinterStatus: (status, color) {
-          print("change printer status: " + status + color);
-        },
-        onPermissionDenied: () => print("onPermissionDenied"));
-    zebraPrinter.rotate();
+    zebraPrinter = widget.printer;
+    notifier = zebraPrinter.notifier;
     zebraPrinter.discoveryPrinters();
-  
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("My Printers"),
-      ),
-      body: printers.isEmpty
-          ? const Center(
-              child: Text("Printers not found"),
-            )
-          : ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                    title: Text(printers[index]["name"]!),
-                    leading: const Icon(Icons.print),
-                    onTap: () {
-                      zebraPrinter
-                          .connectToGenericPrinter(printers[index]["ipAddress"]!);
-                    });
-              },
-              itemCount: printers.length),
-    );
+        appBar: AppBar(
+          title: const Text("My Printers"),
+        ),
+        body: ListenableBuilder(
+          listenable: zebraPrinter.notifier,
+          builder: (context, child) {
+            final printers = notifier.printers;
+            if (printers.isEmpty) {
+              return const Center(
+                child: Text("Printers not found"),
+              );
+            }
+            return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                      title: Text(printers[index].name),
+                      subtitle: Text(printers[index].status,
+                          style: TextStyle(color: printers[index].color)),
+                      leading: Icon(Icons.print, color: printers[index].color),
+                      onTap: () {
+                        zebraPrinter.connectToPrinter(printers[index].address);
+                      });
+                },
+                itemCount: printers.length);
+          },
+        ));
   }
 }
