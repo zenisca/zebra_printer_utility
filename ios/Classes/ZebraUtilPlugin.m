41379425f98ac7e512fde0d96122fc797f36a39b
Manjeet Rangi <zenisca@users.noreply.github.com>
2024-12-06 13:41:06 -0500

    Update ZebraUtilPlugin.m

--- ios/Classes/ZebraUtilPlugin.m
+++ ios/Classes/ZebraUtilPlugin.m
@@ -1,1 +1,17 @@
+#import "ZebraUtilPlugin.h"
+#import "TcpPrinterConnection.h"
+#if __has_include(<zebrautil/zebrautil-Swift.h>)
+#import <zebrautil/zebrautil-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "zebrautil-Swift.h"
+#endif
 
+@implementation ZebraUtilPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+//    TcpPrinterConnection *con =  [TcpPrinterConnection init];
+  [SwiftZebraUtilPlugin registerWithRegistrar:registrar];
+}
+@end
